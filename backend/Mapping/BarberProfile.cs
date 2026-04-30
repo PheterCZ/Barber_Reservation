3bb8263be636192e3df23d82ed49b59c79120541
Petr Navratil <petr.filip.navratil@seznam.cz>
2026-04-30 16:41:05 +0200

    add mapping

---
+++ backend/Mapping/BarberProfile.cs
@@ -0,0 +1,15 @@
+
+using AutoMapper;
+using backend.DTOs;
+using backend.Models;
+
+namespace backend.Mapping
+{
+    public class BarberProfile : Profile
+    {
+        public BarberProfile()
+        {
+            CreateMap<Barber, BarberDto>().ReverseMap();
+        }
+    }
+}
